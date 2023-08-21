deb5b0e7b959330a18f7a39b2c5c4168cb572761
Bharathraja2803 <bharathrajakrishnan@gmail.com>
2023-08-21 09:45:42 +0530

    upto Task class completed

---
+++ Task.java
@@ -0,0 +1,104 @@
+package challenge_set_operations;
+
+import java.util.Objects;
+
+enum Priority {HIGH, MEDIUM, LOW}
+
+enum Staus{INQUEUE, ASSIGNED, INPROGRESS}
+
+public class Task implements Comparable<Task>{
+	private String project;
+	private String description;
+	private String assignee;
+	private Priority priority;
+	private Staus staus;
+	
+	public Task(String project, String description, String assignee, Priority priority, Staus staus) {
+		this.project = project;
+		this.description = description;
+		this.assignee = assignee;
+		this.priority = priority;
+		this.staus = staus;
+	}
+
+	public Task(String project, String description, String assignee, Priority priority) {
+		this(project, description, assignee, priority, assignee == null ? Staus.INQUEUE : Staus.ASSIGNED);
+	}
+
+	public Task(String project, String description, Priority priority) {
+		this(project, description, null, priority);
+	}
+
+	public String getProject() {
+		return project;
+	}
+
+	public void setProject(String project) {
+		this.project = project;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAssignee() {
+		return assignee;
+	}
+
+	public void setAssignee(String assignee) {
+		this.assignee = assignee;
+	}
+
+	public Priority getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Priority priority) {
+		this.priority = priority;
+	}
+
+	public Staus getStaus() {
+		return staus;
+	}
+
+	public void setStaus(Staus staus) {
+		this.staus = staus;
+	}
+
+	@Override
+	public String toString() {
+		return "%-20s %-25s %-8s %-20s %-10s".formatted(project, description, priority.name(), assignee, staus.name());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, priority);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		return Objects.equals(description, other.description) && priority == other.priority;
+	}
+
+	@Override
+	public int compareTo(Task o) {
+		int result = this.project.compareTo(o.project);
+		if(result ==0) {
+			result = this.description.compareTo(o.description);
+		}
+		return result;
+	}
+	
+	
+}
