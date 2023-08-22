2ba5a3b0c774ede77146307894ed9e5ad8b9dc3f
Bharathraja2803 <bharathrajakrishnan@gmail.com>
2023-08-22 11:02:22 +0530

    challenge_set_operation_own_solution

--- Main.java
+++ Main.java
@@ -3,17 +3,48 @@ package challenge_set_operations;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Main {
 	public static void main (String[] args) {
 		Set<Task> tasks = TaskData.getTask("all");
-		sortAndPrint("All Tasks", tasks);
+		sortAndPrint("All Tasks"+"Count: %s".formatted(tasks.size()), tasks);
 		
 		Comparator<Task> sortByPriority = Comparator.comparing(Task::getPriority);
 		Set<Task> annsTask = TaskData.getTask("ann");
-		sortAndPrint("Ann's Tasks", annsTask,sortByPriority);
+		sortAndPrint("Ann's Tasks"+"Count: %s".formatted(annsTask.size()), annsTask,sortByPriority);
+		
+		Set<Task> unionSet = getUnion(List.of(TaskData.getTask("All"), TaskData.getTask("ann"), TaskData.getTask("bob"), TaskData.getTask("carol")));
+		sortAndPrint("Union of all the sets "+"Count: %s".formatted(unionSet.size()), unionSet);
+		
+		Set<Task> intersectAllAnn = getIntersection(TaskData.getTask("All"),TaskData.getTask("ann"));
+		sortAndPrint("Intersection of all the sets and Ann"+"Count: %s".formatted(intersectAllAnn.size()), intersectAllAnn);
+		
+		Set<Task> differenceAllAnn = getDifference(TaskData.getTask("All"), TaskData.getTask("ann"));
+		sortAndPrint("Difference of all the sets and Ann"+"Count: %s".formatted(differenceAllAnn.size()), differenceAllAnn);
+		
+	}
+	
+	public static Set<Task> getUnion(Collection<Set> listOfTaskSet){
+		Set<Task> unionSet = new HashSet<>();
+		for (Set<Task> individualSet : listOfTaskSet) {
+			unionSet.addAll(individualSet);
+		}
+		return unionSet;
+	}
+	
+	public static Set<Task> getIntersection(Set<Task> firstSetOfTask, Set<Task> secondSetOfTask){
+		Set<Task> intersectionSet = new HashSet<>(firstSetOfTask);
+		intersectionSet.retainAll(secondSetOfTask);
+		return intersectionSet;
+	}
+	
+	public static Set<Task> getDifference(Set<Task> firstSetOfTask, Set<Task> secondSetOfTask){
+		Set<Task> differenceSet = new HashSet<>(firstSetOfTask);
+		differenceSet.removeAll(secondSetOfTask);
+		return differenceSet;
 	}
 	
 	public static void sortAndPrint(String header, Collection<Task> taskList) {
